441373ddbd40170bc98df060b8817361c6ec788a
Aleksandar Pesic <peske.nis@gmail.com>
2019-02-13 16:08:12 +0100

    Simplifying callback.
    Signed-off-by: Aleksandar Pesic <peske.nis@gmail.com>

--- interface_change_handler_test.go
+++ interface_change_handler_test.go
@@ -10,43 +10,47 @@ import (
 	"testing"
 )
 
-func TestRegisterUnregisterInterfaceChangeCallback(t *testing.T) {
+// Example callback
+var interfaceChangeCallbackExample InterfaceChangeCallback = func (ifc *MibIpinterfaceRow, notificationType MibNotificationType) {
 
-	var c InterfaceChangeCallback = interfaceChangeTestCallback
-
-	if InterfaceChangeCallbackRegistered(&c) {
-		t.Error("InterfaceChangeCallbackRegistered returns true although nothing is registered.")
-		return;
-	}
-
-	err := RegisterInterfaceChangeCallback(&c)
-
-	if err != nil {
-		t.Errorf("RegisterInterfaceChangeCallback returned error: %v", err)
-		return;
-	}
-
-	if !InterfaceChangeCallbackRegistered(&c) {
-		t.Error("InterfaceChangeCallbackRegistered returns false although a callback is registered successfully.")
-	}
-
-	err = UnregisterInterfaceChangeCallback(&c)
-
-	if err != nil {
-		t.Errorf("UnregisterInterfaceChangeCallback returned error: %v", err)
-		return;
-	}
-
-	if InterfaceChangeCallbackRegistered(&c) {
-		t.Error("InterfaceChangeCallbackRegistered returns true although the callback is unregistered successfully.")
-	}
-}
-
-func interfaceChangeTestCallback(ifc *MibIpinterfaceRow, notificationType MibNotificationType) {
 	fmt.Printf(`=========================== INTERFACE CHANGED START ===========================
 MibNotificationType: %s
 MibIpinterfaceRow:
 %s
 ============================ INTERFACE CHANGED END ============================
 `, notificationType.String(), toIndentedText(ifc.String(), "  "))
+
+}
+
+func TestRegisterUnregisterInterfaceChangeCallback(t *testing.T) {
+
+	if InterfaceChangeCallbackRegistered(&interfaceChangeCallbackExample) {
+		t.Error("InterfaceChangeCallbackRegistered returns true although nothing is registered.")
+		return;
+	}
+
+	err := RegisterInterfaceChangeCallback(&interfaceChangeCallbackExample)
+
+	if err != nil {
+		t.Errorf("RegisterInterfaceChangeCallback returned error: %v", err)
+		return;
+	}
+
+	if !InterfaceChangeCallbackRegistered(&interfaceChangeCallbackExample) {
+		t.Error("InterfaceChangeCallbackRegistered returns false although a callback is registered successfully.")
+	}
+
+	err = UnregisterInterfaceChangeCallback(&interfaceChangeCallbackExample)
+
+	if err != nil {
+		t.Errorf("UnregisterInterfaceChangeCallback returned error: %v", err)
+		return;
+	}
+
+	if InterfaceChangeCallbackRegistered(&interfaceChangeCallbackExample) {
+		t.Error("InterfaceChangeCallbackRegistered returns true although the callback is unregistered successfully.")
+	}
+}
+
+func interfaceChangeCallbackFunctionExample(ifc *MibIpinterfaceRow, notificationType MibNotificationType) {
 }
